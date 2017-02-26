e8c6a494d202df7ec7348e8a61f7e0699194fe32
Paty91 <paf_91@outlook.com>
2017-02-26 18:29:20 +0100

    Enlace BootStrap
    Enlace BootStrap corregido

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -4,13 +4,11 @@
 	<title>Concurso Fotografía</title>
 	<!--Rutas-->
 	<!-- link minified CSS -->
-	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
-
+	<link rel="stylesheet" href="css/bootstrap.min.css">
 	<link rel="stylesheet" href="css/cssPersonalizado.css">
 	  <link rel="stylesheet" href="css/magic.css">
 
 	<!-- link JQUERY -->
-	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
     <script src="js/jquery.js"></script>
     <script src="js/jquery.waypoints.js"></script>
 
