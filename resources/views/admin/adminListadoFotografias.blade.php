2f8fa4ab73da39860dea430cc0ef47a906ae2d3f
Paty91 <paf_91@outlook.com>
2017-04-11 16:00:14 +0200

    FormularioListadoFotos

--- resources/views/admin/adminListadoFotografias.blade.php
+++ resources/views/admin/adminListadoFotografias.blade.php
@@ -1,4 +1,39 @@
 @extends('admin.layoutadmin')
+@section('linkCss')
+<link rel="stylesheet" href="{{asset('css/listadoFotos.css')}}">
+@endsection
 @section('contenidoAdmin')
+<div class="col-md-4">
+ <form method="POST" action="{{url('auth/register')}}">
+   {!! csrf_field() !!}
+    <div class="form-group">
+    <label for="categoria">Tipo de usuario:</label>
+    <select class="form-control" name="categoria">
+      <option value="alumno" name="alumno">Todos</option>
+      <option value="alumno" name="alumno">Alumno/Alumna</option>
+      <option value="tutor" name="tutor">Padre/Madre</option>
+      <option value="profesor" name="profesor">Profesor/Profesora</option>
+    </select>
+    </div>
+</form>
+</div>
 
+<div class="col-md-4">
+ <form method="POST" action="{{url('auth/register')}}">
+   {!! csrf_field() !!}
+    <div class="form-group">
+    <label for="categoria">Resultados</label>
+    <input type="number" name="numResultados" min="0" value="10">
+    </div>
+</form>
+</div>
+
+<div class="col-md-4">
+ <form method="POST" action="{{url('auth/register')}}">
+   {!! csrf_field() !!}
+    <div class="form-group">
+     <button type="submit" class="btn-primary btn-lg active btnRegistrarse btnFormulario btnLogin btnFiltrar">Filtrar</button>
+    </div>
+</form>
+</div>
 @endsection
