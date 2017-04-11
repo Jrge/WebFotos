0faadd504efee782525f794f5574519029bf6b47
Paty91 <paf_91@outlook.com>
2017-04-11 20:04:04 +0200

    CambioFormulario

--- resources/views/admin/adminListadoFotografias.blade.php
+++ resources/views/admin/adminListadoFotografias.blade.php
@@ -4,7 +4,7 @@
 @endsection
 @section('contenidoAdmin')
 <div class="col-md-4">
- <form method="POST" action="{{url('auth/register')}}">
+ <form method="POST" action="{{url('adminListadoFotografias')}}">
    {!! csrf_field() !!}
     <div class="form-group">
     <label for="categoria">Tipo de usuario:</label>
@@ -15,21 +15,17 @@
       <option value="profesor" name="profesor">Profesor/Profesora</option>
     </select>
     </div>
-</form>
 </div>
 
 <div class="col-md-4">
- <form method="POST" action="{{url('auth/register')}}">
    {!! csrf_field() !!}
     <div class="form-group">
     <label for="categoria">Resultados</label>
     <input type="number" name="numResultados" min="0" value="10">
     </div>
-</form>
 </div>
 
 <div class="col-md-4">
- <form method="POST" action="{{url('adminListadoFotografias')}}">
    {!! csrf_field() !!}
     <div class="form-group">
      <button type="submit" class="btn-primary btn-lg active btnRegistrarse btnFormulario btnFiltrar">Filtrar</button>
